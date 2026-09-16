59c7183f34a3f51c7ed7ed7e5e8b9a52c4e08e32
Krishna <raki74718@gmail.com>
2026-09-16 13:01:51 +0530

    Enable CORS for Netlify frontend

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const path = require("path");
 const crypto = require("crypto");
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 const PORT = 3000;
 
 // ===============================
